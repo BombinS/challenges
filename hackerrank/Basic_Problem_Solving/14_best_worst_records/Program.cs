a2e787e4c5737f93101b6efffb5a892e1b6a795c
Sergey Bombin <sergey.bombin.1977@gmail.com>
2020-11-24 17:37:07 +0300

    second day problem solving

---
+++ hackerrank/Basic_Problem_Solving/14_best_worst_records/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _14_best_worst_records
+{
+  class Program
+  {
+    // Complete the breakingRecords function below.
+    static int[] breakingRecords(int[] scores)
+    {
+      int best, worst;
+      int bestResult = 0;
+      int worstResult = 0;
+      best = worst = scores[0];
+      for (int i = 1; i < scores.Length; i++) 
+      {
+        if (scores[i] > best)
+        {
+          bestResult++;
+          best = scores[i];
+        }
+        else if (scores[i] < worst)
+        {
+          worstResult++;
+          worst = scores[i];
+        }
+      }
+      return new int[] { bestResult, worstResult};
+    }
+
+    static void Main(string[] args)
+    {
+      int n = Convert.ToInt32(Console.ReadLine());
+      int[] scores = Array.ConvertAll(Console.ReadLine().Split(' '), scoresTemp => Convert.ToInt32(scoresTemp));
+      int[] result = breakingRecords(scores);
+      Console.WriteLine(string.Join(" ", result));
+    }
+  }
+}
---
+++ hackerrank/Basic_Problem_Solving/15_birthday_bar/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace _15_birthday_bar
+{
+  class Program
+  {
+    // Complete the birthday function below.
+    static int birthday(List<int> s, int d, int m)
+    {
+      int counter = 0;
+      int i = 0;
+      while (i + m <= s.Count) 
+      {
+        int sum = 0;
+        for (int j = 0; j < m; j++) 
+        {
+          sum += s[i + j];
+        }
+        if (sum == d) 
+        {
+          counter++;
+        }
+        i++;
+      }
+      return counter;
+    }
+
+    static void Main(string[] args)
+    {
+      int n = Convert.ToInt32(Console.ReadLine().Trim());
+      List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+      string[] dm = Console.ReadLine().TrimEnd().Split(' ');
+      int d = Convert.ToInt32(dm[0]);
+      int m = Convert.ToInt32(dm[1]);
+      int result = birthday(s, d, m);
+      Console.WriteLine(result);
+
+    }
+  }
+}
