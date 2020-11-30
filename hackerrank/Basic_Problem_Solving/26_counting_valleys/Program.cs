67596497533d6ca11997bc87e27747f61b0b45b9
Sergey Bombin <sergey.bombin.1977@gmail.com>
2020-11-30 16:26:26 +0300

    sixth day of problem solving

---
+++ hackerrank/Basic_Problem_Solving/26_counting_valleys/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace _26_counting_valleys
+{
+
+  class Result
+  {
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, string path)
+    {      
+      char[] splitted = path.ToCharArray();
+      int prevLevel = 0;
+      int thisLevel = 0;
+      int valleyCount = 0;
+      for (int i = 0; i < splitted.Length; i++)
+      {
+        if (splitted[i] == 'U')
+          thisLevel++;
+        else
+          thisLevel--;
+
+        if (thisLevel == -1 && prevLevel == 0)
+          valleyCount++;
+
+        prevLevel = thisLevel;
+      }
+      return valleyCount;
+    }
+
+  }
+
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      int steps = Convert.ToInt32(Console.ReadLine().Trim());
+      string path = Console.ReadLine();
+      int result = Result.countingValleys(steps, path);
+      Console.WriteLine(result);
+    }
+  }
+}
---
+++ hackerrank/Basic_Problem_Solving/30_utopian_tree/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace _30_utopian_tree
+{
+
+  class Program
+  {
+    // Complete the utopianTree function below.
+    static int utopianTree(int n)
+    {
+      int height = 1;
+      for (int i = 1; i <= n; i++)
+      {
+        if (i % 2 != 0)
+          height *= 2;
+        else
+          height += 1;
+      }
+      return height;
+    }
+
+    static void Main(string[] args)
+    {
+      int t = Convert.ToInt32(Console.ReadLine());
+
+      for (int tItr = 0; tItr < t; tItr++)
+      {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int result = utopianTree(n);
+        Console.WriteLine(result);
+      }
+    }
+  }
+}
