dd0eb9c02edd698e5bf026ca139db8a03e0d853f
Sergey Bombin <sergey.bombin.1977@gmail.com>
2022-03-10 17:06:21 +0300

    remove previous tasks

--- hackerrank/Practice_Python_Introduction/python-loops.py
+++
@@ -1,6 +0,0 @@
-# https://www.hackerrank.com/challenges/python-loops/problem
-
-if __name__ == '__main__':
-    n = int(input())
-    for x in range(n):
-        print(x*x)
--- hackerrank/Practice_Python_Introduction/python-print.py
+++
@@ -1,8 +0,0 @@
-# https://www.hackerrank.com/challenges/python-print/problem
-
-from __future__ import print_function
-
-if __name__ == '__main__':
-    n = int(input())
-    for x in range(1, n + 1):
-        print(x, end='')
